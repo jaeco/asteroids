6ece9039c7e88f1443591b66c75002675d954cc5
James Corkran <james.e.corkran@gmail.com>
2025-11-02 05:21:12 -0500

    Adding FPS at 60fps

--- main.py
+++ main.py
@@ -4,18 +4,15 @@ from constants import *
 def main():
     game.init()
     screen = game.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = game.time.Clock()
+    dt = 0
     while True:
         for event in game.event.get():
             if event.type == game.QUIT:
                 return
         screen.fill("Black")
         game.display.flip()
-    
-    
-    #print("Starting Asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
-
-
+        clock.tick(60)
+        dt = clock.tick() / 1000
 if __name__ == "__main__":
     main()
