ac1559de1eeec21f6db39acb366ac01a8492fd5c
James Corkran <james.e.corkran@gmail.com>
2025-11-02 05:53:47 -0500

    Player sprite class, logic, and addition to screen

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import pygame as game
+from player import Player
 from constants import *
 
 def main():
     game.init()
     screen = game.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = game.time.Clock()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     dt = 0
     while True:
         for event in game.event.get():
             if event.type == game.QUIT:
                 return
         screen.fill("Black")
+        player.draw(screen)
         game.display.flip()
         clock.tick(60)
         dt = clock.tick() / 1000
